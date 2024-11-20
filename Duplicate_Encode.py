e09a4af701d647735e31a5b2c56660da3982d0ed
Kaneki <alex123viri@gmail.com>
2024-11-20 19:32:55 +0100

    test(Codewars): Arreglado el testing para quitar los warning.

--- Duplicate_Encode.py
+++ Duplicate_Encode.py
@@ -17,11 +17,11 @@ def duplicate_encode(word):
 import unittest
 class Testeo(unittest.TestCase):
     
-    def test_duplicate_encode(test):
-        test.assertEqual(duplicate_encode("din"),"(((")
-        test.assertEqual(duplicate_encode("recede"),"()()()")
-        test.assertEqual(duplicate_encode("Success"),")())())","should ignore case")
-        test.assertEqual(duplicate_encode("(( @"),"))((")
+    def test_duplicate_encode(self):
+        self.assertEqual(duplicate_encode("din"),"(((")
+        self.assertEqual(duplicate_encode("recede"),"()()()")
+        self.assertEqual(duplicate_encode("Success"),")())())","should ignore case")
+        self.assertEqual(duplicate_encode("(( @"),"))((")
 
 if __name__ == '__main__':
     unittest.main()

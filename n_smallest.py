151244bf4b23d1be1a8705173308fe027363c74b
KanekiiKat <alex123viri@gmail.com>
2024-11-20 11:24:42 +0100

    fix(Tareas de Codewars): Arreglados los test.

--- n_smallest.py
+++ n_smallest.py
@@ -18,17 +18,17 @@ def first_n_smallest(arr, n):
 import unittest
 class Testeo(unittest.TestCase):
 
-    def test_first_n_smallest(test):
-        test.assertEqual(first_n_smallest([1,2,3,4,5],3), [1,2,3])
-        test.assertEqual(first_n_smallest([5,4,3,2,1],3), [3,2,1])
-        test.assertEqual(first_n_smallest([1,2,3,1,2],3), [1,2,1])
-        test.assertEqual(first_n_smallest([1,2,3,-4,0],3), [1,-4,0])
-        test.assertEqual(first_n_smallest([1,2,3,4,5],0), [])
-        test.assertEqual(first_n_smallest([1,2,3,4,5],5), [1,2,3,4,5])
-        test.assertEqual(first_n_smallest([1,2,3,4,2],4), [1,2,3,2])
-        test.assertEqual(first_n_smallest([2,1,2,3,4,2],2), [2,1])
-        test.assertEqual(first_n_smallest([2,1,2,3,4,2],3), [2,1,2])
-        test.assertEqual(first_n_smallest([2,1,2,3,4,2],4), [2,1,2,2])
+    def test_first_n_smallest(self):
+        self.assertEqual(first_n_smallest([1,2,3,4,5],3), [1,2,3])
+        self.assertEqual(first_n_smallest([5,4,3,2,1],3), [3,2,1])
+        self.assertEqual(first_n_smallest([1,2,3,1,2],3), [1,2,1])
+        self.assertEqual(first_n_smallest([1,2,3,-4,0],3), [1,-4,0])
+        self.assertEqual(first_n_smallest([1,2,3,4,5],0), [])
+        self.assertEqual(first_n_smallest([1,2,3,4,5],5), [1,2,3,4,5])
+        self.assertEqual(first_n_smallest([1,2,3,4,2],4), [1,2,3,2])
+        self.assertEqual(first_n_smallest([2,1,2,3,4,2],2), [2,1])
+        self.assertEqual(first_n_smallest([2,1,2,3,4,2],3), [2,1,2])
+        self.assertEqual(first_n_smallest([2,1,2,3,4,2],4), [2,1,2,2])
 
 
 if __name__ == '__main__':

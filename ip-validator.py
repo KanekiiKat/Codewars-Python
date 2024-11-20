d9281ccd74867005c1744adca98c5521a6509086
Kaneki <alex123viri@gmail.com>
2024-11-20 16:43:19 +0100

    test(Codewars): He mejorado el testing de ip-validator.py para quitar los warning.

--- ip-validator.py
+++ ip-validator.py
@@ -2,7 +2,7 @@
 
 # Codigo:
 
-def is_valid_IP(strng):
+def is_valid_ip(strng):
     points = strng.split(".")
     if len(points) != 4:
         return False
@@ -24,20 +24,19 @@ def is_valid_IP(strng):
 import unittest
 class Testeo(unittest.TestCase):
     
-    def test_is_valid_IP(test):
-        test.assertEqual(is_valid_IP('12.255.56.1'),     True)
-        test.assertEqual(is_valid_IP(''),                False)
-        test.assertEqual(is_valid_IP('abc.def.ghi.jkl'), False)
-        test.assertEqual(is_valid_IP('123.456.789.0'),   False)
-        test.assertEqual(is_valid_IP('12.34.56'),        False)
-        test.assertEqual(is_valid_IP('12.34.56 .1'),     False)
-        test.assertEqual(is_valid_IP('12.34.56.-1'),     False)
-        test.assertEqual(is_valid_IP('123.045.067.089'), False)
-        test.assertEqual(is_valid_IP('127.1.1.0'),        True)
-        test.assertEqual(is_valid_IP('0.0.0.0'),          True)
-        test.assertEqual(is_valid_IP('0.34.82.53'),       True)
-        test.assertEqual(is_valid_IP('192.168.1.300'),   False)
+    def test_is_valid_ip(self):
+        self.assertTrue(is_valid_ip('12.255.56.1'))
+        self.assertFalse(is_valid_ip(''))
+        self.assertFalse(is_valid_ip('abc.def.ghi.jkl'))
+        self.assertFalse(is_valid_ip('123.456.789.0'))
+        self.assertFalse(is_valid_ip('12.34.56'))
+        self.assertFalse(is_valid_ip('12.34.56 .1'))
+        self.assertFalse(is_valid_ip('12.34.56.-1'))
+        self.assertFalse(is_valid_ip('123.045.067.089'))
+        self.assertTrue(is_valid_ip('127.1.1.0'))
+        self.assertTrue(is_valid_ip('0.0.0.0'))
+        self.assertTrue(is_valid_ip('0.34.82.53'))
+        self.assertFalse(is_valid_ip('192.168.1.300'))
 
-        
 if __name__ == '__main__':
     unittest.main()
